b10a3f0fbc8b79cbf2d2dc1ea105042ad2ac675e
jordisilvazup <jordi.silva@zup.com.br>
2021-09-08 17:04:48 -0300

    setup

---
+++ src/main/java/br/com/zup/edu/desafiopagamentos/DesafiopagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.desafiopagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/desafiopagamentos/DesafiopagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.desafiopagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
